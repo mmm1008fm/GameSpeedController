9dd64950be434e02310359668601c5ca474072c7
mmm1008fm <44507247+mmm1008fm@users.noreply.github.com>
2025-06-17 02:27:39 +0300

    Add hotkey manager with global events

--- Launcher/HotkeyManager.cs
+++ Launcher/HotkeyManager.cs
@@ -13,23 +13,30 @@ namespace Launcher
         [DllImport("user32.dll")]
         private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
 
-        private const int HOTKEY_ID_TOGGLE = 1;
-        private const int HOTKEY_ID_RESET = 2;
+        private const int HOTKEY_ID_INCREASE = 1;
+        private const int HOTKEY_ID_DECREASE = 2;
+        private const int HOTKEY_ID_TOGGLE_PAUSE = 3;
 
         private const uint MOD_CONTROL = 0x0002;
-        private const uint VK_F1 = 0x70;
-        private const uint VK_F2 = 0x71;
+        private const uint VK_F9 = 0x78;
+        private const uint VK_F10 = 0x79;
+        private const uint VK_F11 = 0x7A;
 
         private Window? window;
         private IntPtr windowHandle;
+
+        public event EventHandler? Increase;
+        public event EventHandler? Decrease;
+        public event EventHandler? TogglePause;
 
         public void RegisterHotkeys(Window window)
         {
             this.window = window;
             windowHandle = new WindowInteropHelper(window).Handle;
 
-            RegisterHotKey(windowHandle, HOTKEY_ID_TOGGLE, MOD_CONTROL, VK_F1);
-            RegisterHotKey(windowHandle, HOTKEY_ID_RESET, MOD_CONTROL, VK_F2);
+            RegisterHotKey(windowHandle, HOTKEY_ID_INCREASE, MOD_CONTROL, VK_F9);
+            RegisterHotKey(windowHandle, HOTKEY_ID_DECREASE, MOD_CONTROL, VK_F10);
+            RegisterHotKey(windowHandle, HOTKEY_ID_TOGGLE_PAUSE, MOD_CONTROL, VK_F11);
 
             ComponentDispatcher.ThreadPreprocessMessage += ComponentDispatcher_ThreadPreprocessMessage;
         }
@@ -38,8 +45,9 @@ namespace Launcher
         {
             if (windowHandle != IntPtr.Zero)
             {
-                UnregisterHotKey(windowHandle, HOTKEY_ID_TOGGLE);
-                UnregisterHotKey(windowHandle, HOTKEY_ID_RESET);
+                UnregisterHotKey(windowHandle, HOTKEY_ID_INCREASE);
+                UnregisterHotKey(windowHandle, HOTKEY_ID_DECREASE);
+                UnregisterHotKey(windowHandle, HOTKEY_ID_TOGGLE_PAUSE);
             }
 
             ComponentDispatcher.ThreadPreprocessMessage -= ComponentDispatcher_ThreadPreprocessMessage;
@@ -51,17 +59,22 @@ namespace Launcher
             {
                 switch (msg.wParam.ToInt32())
                 {
-                    case HOTKEY_ID_TOGGLE:
-                        // TODO: Implement toggle time scaling
+                    case HOTKEY_ID_INCREASE:
+                        Increase?.Invoke(this, EventArgs.Empty);
                         handled = true;
                         break;
 
-                    case HOTKEY_ID_RESET:
-                        // TODO: Implement reset time multiplier
+                    case HOTKEY_ID_DECREASE:
+                        Decrease?.Invoke(this, EventArgs.Empty);
+                        handled = true;
+                        break;
+
+                    case HOTKEY_ID_TOGGLE_PAUSE:
+                        TogglePause?.Invoke(this, EventArgs.Empty);
                         handled = true;
                         break;
                 }
             }
         }
     }
-} 
+}
